364aa8468a3d0d66123d7b8c2b6dc7ebca9bb03c
Edward <comgeek71@gmail.com>
2026-01-07 17:45:49 -0600

    feature/report: implemented the report job and schedule it using command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule daily sales report to run every evening at 8 PM
+Schedule::command('sales:generate-daily-report')
+    ->dailyAt('20:00')
+    ->withoutOverlapping()
+    ->runInBackground();
