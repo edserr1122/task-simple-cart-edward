91224d78c16af575f88f6cf958b1c4df4bdde8d3
Edward <comgeek71@gmail.com>
2026-01-07 13:26:00 -0600

    feature/models: added product, cart and cartitem models

---
+++ app/Models/Product.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'stock_quantity',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'stock_quantity' => 'integer',
+        ];
+    }
+
+    /**
+     * Get the cart items for the product.
+     */
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+}
+
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,12 @@ class User extends Authenticatable
             'two_factor_confirmed_at' => 'datetime',
         ];
     }
+
+    /**
+     * Get the cart for the user.
+     */
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
 }
